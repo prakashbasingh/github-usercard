467f2ea8ab1b43ec17c456ee32efef5ec866a765
unknown <prakashbahadursingh2@gmail.com>
2020-04-14 01:56:30 -0700

    text conten added to the elements

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,16 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+
 axios.get('https://api.github.com/users/prakashbasingh')
+.then (success => {
+  // console.log(success.data)
+// debugger
+})
+.catch(failure => {
+  // console.log(failure)
+})
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -14,6 +23,9 @@ axios.get('https://api.github.com/users/prakashbasingh')
            create a new component and add it to the DOM as a child of .cards
 */
 
+
+
+
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
           , manually find some other users' github handles, or use the list found 
@@ -24,7 +36,11 @@ axios.get('https://api.github.com/users/prakashbasingh')
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+// const followersArray = [  'tetondan', 'dustinmyers',  'justsml',  'luishrd',  'bigknell'];
+
+
+
+       
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -54,50 +70,46 @@ const followersArray = [];
   bigknell
 
 */
+ function userCard(object){
+  // creating element
+    const cardDiv = document.createElement('div')
+    const userImg = document.createElement('img')
+    const cardInfoDiv = document.createElement('div')
+    const nameH3 = document.createElement('h3')
+    const userNameP = document.createElement('p')
+    const locationP = document.createElement('p')
+    const profileP = document.createElement('p')
+    const profileLink = document.createElement('a')
+    const followerP = document.createElement('p')
+    const followingP = document.createElement('p')
+    const bioP = document.createElement('p')
 
-function userCard({avatarURL, name, userName, location, htmlURL, following, bio}){
-// creating element
-const cardDiv = document.createElement('div')
-const userImg = document.createElement('img')
-const cardInfoDiv = document.createElement('div')
-const nameH3 = document.createElement('h3')
-const userNameP = document.createElement('p')
-const locationP = document.createElement('p')
-const profileP = document.createElement('p')
-const userLink = document.createElement('a')
-const followerP = document.createElement('p')
-const followingP = document.createElement('p')
-const bioP = document.createElement('p')
+    // creating element structure 
+    cardDiv.appendChild(userImg)
+    cardDiv.appendChild(cardInfoDiv)
+    cardInfoDiv.appendChild(nameH3)
+    cardInfoDiv.appendChild(userNameP)
+    cardInfoDiv.appendChild(locationP)
+    cardInfoDiv.appendChild(profileP)
+    profileP.appendChild(profileLink)
+    cardInfoDiv.appendChild(followerP)
+    cardInfoDiv.appendChild(followingP)
+    cardInfoDiv.appendChild(bioP)
 
-// creating element structure 
-cardDiv.appendChild.apply(userImg)
-cardDiv.appendChild.apply(cardInfoDiv)
-cardInfoDiv.appendChild(nameH3)
-cardInfoDiv.appendChild(userNameP)
-cardInfoDiv.appendChild(locationP)
-cardInfoDiv.appendChild(profileP)
-profileP.appendChild(userLink)
-cardInfoDiv.appendChild(followerP)
-cardInfoDiv.appendChild(followingP)
-cardInfoDiv.appendChild(bioP)
+    // giving class name/attributes
+    cardDiv.classList.add('card')
+    cardInfoDiv.classList.add('card-info')
+    nameH3.classList.add('name')
+    userNameP.classList.add('username')
+    userImg.src = object.avatar_url
+    profileLink.href = object.html_url
+    // setting content
+    nameH3.textContent = 'Name: ' + object.name
+    userNameP.textContent = 'Username: ' + object.login
+    locationP.textContent = 'Location: ' + object.location
+    followerP.textContent = 'Follower: ' + object.followers
+    followingP.textContent = 'Following: ' + object.following
+    bioP.textContent = 'Bio: ' + object.bio
 
-// giving class name/attributes
-cardDiv.classList.add('card')
-userImg.src = imgURL
-cardInfoDiv.classList.add('card-info')
-nameH3.classList.add('name')
-userNameP.classList.add('username')
-profileP.href = htmlURL
-
-// setting content
-personName.textContent = Name
-userName.textContent = `User Name: ${login}`
-userLocation.textContent = Location
-userProfileLine.textContent = htmlURL
-userFollowers.textContent = `Followers: ${followers}`
-userFollowing.textContent = `Following: ${following}`
-userBio.textContent = `Bio: ${bio}`
-
-
-
+  return cardDiv
 }
